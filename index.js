2ad040fbd4cb257a4a88b4fbd64f86941218a1ad
Artur <arturdantasrodrigues@gmail.com>
2022-03-07 19:55:19 -0300

    Redirecionamento já funciona
    o usuário informe um texto, e eu redireciono elel para uma página do google utilizando o texto informado

--- index.js
+++ index.js
@@ -5,18 +5,13 @@ inputEl.addEventListener("keydown", function(e){
    if(e.code === "Enter") {
       let text = inputEl.value
       text = formatando(text)
-      inputEl.value = text
+      pesquisa += text
+      inputEl.value = ""
+      window.location.href = pesquisa
    }
 })
 
-function formatando(texto) {
-   for(let i = 0; i < texto.length; i++){
-      console.log(texto[i])
-      if(texto[i] === " "){
-         texto[i] = "+"
-      }
-   }
-   console.log(texto[5])
-
-   return texto
+function formatando(text) {
+   text = text.replace(/ /g, "+")
+   return text
 }

c4df0b0851d6fa6d29ada2ec9d48539b0c0298ea
Artur <arturdantasrodrigues@gmail.com>
2022-03-08 14:50:48 -0300

    Agora temos um histórico
    quando eu clico na barra de pesquisa aparecem algumas pesquisas que eu já fiz anteriormente, pois eu guardei isso tudo no LocalStorage

--- index.js
+++ index.js
@@ -1,21 +1,44 @@
 const localHistory = localStorage.getItem("history")
 const inputEl = document.getElementById("input-el")
+const historyEl = document.querySelector(".history")
 let history = []
-let pesquisa = "https://www.google.com/search?q=";
+
 if (localHistory){
    history = JSON.parse(localHistory)
 }
 
 inputEl.addEventListener("keydown", function(e){
    if(e.code === "Enter") {
-      let text = inputEl.value
-      history.push(text)
+      let url = inputEl.value
+      history.push(url)
       localStorage.setItem("history", JSON.stringify(history))
-      text = formatando(text)
-      pesquisa += text
-      inputEl.value = ""
-      window.location.href = pesquisa
+      redirect(url)
    }
+})
+
+function redirect(url) {
+   let pesquisa = "https://www.google.com/search?q=";
+   url = formatando(url)
+   pesquisa += url
+   inputEl.value = ""
+   window.location.href = pesquisa
+}
+
+inputEl.addEventListener("focus", function(){
+   historyEl.innerHTML = `<ul>`
+
+   for(let i = 0; i < history.length; i++)
+      historyEl.innerHTML += `
+      <li class="history-item" onclick="redirect('${history[i]}')">
+         ${history[i]}
+      </li>`
+
+   historyEl.innerHTML += `</ul>`
+   historyEl.classList.add("active")
+})
+
+inputEl.addEventListener("blur", function(){
+   historyEl.classList.remove("active")
 })
 
 function formatando(text) {

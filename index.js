03b1c47dd964fb7962c7bd6dc48af1b834f8d5d7
Artur <arturdantasrodrigues@gmail.com>
2022-03-08 13:20:48 -0300

    styles
    just maked some adjusts

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
+const localHistory = localStorage.getItem("history")
 const inputEl = document.getElementById("input-el")
+let history = []
 let pesquisa = "https://www.google.com/search?q=";
+if (localHistory){
+   history = JSON.parse(localHistory)
+}
 
 inputEl.addEventListener("keydown", function(e){
    if(e.code === "Enter") {
       let text = inputEl.value
+      history.push(text)
+      localStorage.setItem("history", JSON.stringify(history))
       text = formatando(text)
       pesquisa += text
       inputEl.value = ""

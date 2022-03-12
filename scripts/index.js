0fbc5ed561c847a2b3fee716525c77c1ea60a3c0
Artur Dantas <72894238+Artur906@users.noreply.github.com>
2022-03-12 08:50:56 -0300

    Adicionando alguns comentários

--- scripts/index.js
+++ scripts/index.js
@@ -4,19 +4,22 @@ const inputEl = document.getElementById("input-el")
 const searchContentEl = document.querySelector(".search-content")
 let history = []
 
+// Verificando se existe alguma pesquisa guardada no local Storage
 if (localHistory){
    history = JSON.parse(localHistory)
 }
 
+// Escutando quando o usuário aperta Enter
 inputEl.addEventListener("keydown", function(e){
    if(e.code === "Enter") {
       search()
    }
 })
 
+// Verificando se o usuário está clicando na área que contem a barra de pesquisa, ou na área do histórico
+// Pra diminuir a quantidade de verificações eu coloquei a classe s-bar em todos os elementos, essa classe só serve para essa verificação
 addEventListener("click", function(e) {
    let element = e.target
-
    if(element.classList.contains("s-bar")){ 
       searchContentEl.classList.add("active")
       historyEl.classList.remove("off")
@@ -27,11 +30,11 @@ addEventListener("click", function(e) {
    }
 })
 
+// inserindo o histórico no HTML
 function renderHistory(){
    historyEl.innerHTML = ""
    for(let i = 0; i < history.length; i++){
       if (i >= 6) return 
-
       historyEl.innerHTML += `
       <div class="link s-bar">
          <img src="images/clock.svg" class="s-bar" alt="clock">
@@ -42,31 +45,13 @@ function renderHistory(){
    }          
 }
 
-function apaga(index){
-   history.splice(index, 1)
-   saveInHistory(null, false)
-   renderHistory()
-}
-
+// funções relacionadas a manipulação do input do usuário
 function search(){
    let url = getInput()
    if(url != ""){
       saveInHistory(url)
       redirect(url)
    } 
-}
-
-function formatando(text) {
-   text = text.replace(/ /g, "+")
-   return text
-}
-
-function redirect(url) {
-   let pesquisa = "https://www.google.com/search?q=";
-   url = formatando(url)
-   pesquisa += url
-   inputEl.value = ""
-   window.location.href = pesquisa
 }
 
 function getInput() {
@@ -79,4 +64,23 @@ function saveInHistory(item, add=true) {
       history.unshift(item)
    }
    localStorage.setItem("history", JSON.stringify(history))
+   renderHistory()
+}
+
+function redirect(url) {
+   let pesquisa = "https://www.google.com/search?q=";
+   url = formatando(url)
+   pesquisa += url
+   inputEl.value = ""
+   window.location.href = pesquisa
+}
+
+function formatando(text) {
+   text = text.replace(/ /g, "+")
+   return text
+}
+
+function apaga(index){
+   history.splice(index, 1)
+   saveInHistory(null, false)
 }
